37c7a531974ee1c3f0b61a7c4d017b5dd1aecf47
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2022-06-13 23:19:00 +0300

    GPS module

--- app/src/main.js
+++ app/src/main.js
@@ -9,8 +9,16 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
+// Google Maps API
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 createApp(App)
   .use(store)
   .use(router)
   .use(BootstrapVue3)
+  .use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyBinH7ZodZEFqMAW8U4DeZ2SN86dWu93d0'
+    }
+  })
   .mount('#app')
--- app/src/views/Map.vue
+++ app/src/views/Map.vue
@@ -3,8 +3,54 @@
     <div class="content-area">
       <div class="container-fluid">
         <h2>Weather Station on Campus</h2>
-        <p>TODO: Add GPS module here!</p>
+        <GMapMap
+          :center="center"
+          :zoom="7"
+          map-type-id="terrain"
+          style="width: 100%; height: 500px"
+        >
+          <GMapCluster>
+            <GMapMarker
+              v-for="marker in markers"
+              :key="marker.id"
+              :position="marker.position"
+              :title="marker.title"
+              :icon="marker.icon"
+            >
+              <GMapInfoWindow>
+                <div>
+                  <h3>{{ marker.title }}</h3>
+                  <p>{{ marker.content }}</p>
+                  <b-button>Test</b-button>
+                </div>
+              </GMapInfoWindow>
+            </GMapMarker>
+          </GMapCluster>
+        </GMapMap>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  setup () {
+    return {
+      center: {
+        lat: 40.7128,
+        lng: -74.0060
+      },
+      markers: [
+        {
+          position: {
+            lat: 40.7128,
+            lng: -74.0060
+          },
+          title: 'New York',
+          content: 'New York City'
+        }
+      ]
+    }
+  }
+}
+</script>
